382009b509f94a5283ef2cac1720084c26902c99
Dylan Buchi <devybuchi@gmail.com>
2020-06-25 11:27:49 -0300

    regular expressions practice

--- regular_expressions/regex.py
+++ regular_expressions/regex.py
@@ -1,14 +1,79 @@
 import re
 
-result = re.search(r"l.l", "lol")
-result = re.search(r"l..l", "lool")
-result = re.search(r"l..l", "yooloololo")
-result = re.search(r"l..l", "heLLOOL", re.IGNORECASE)
 
-python = "python"
-java = "Java"
+def search_pattern(regex, string, specialcase=re.UNICODE):
+    # search pattern regex in the string, optional arg unicode by default
+    result = re.search(regex, string, specialcase)
+    return result
 
-result = re.search(r"[pP]ython", python)
-result = re.search(r"[jJ]ava", java)
 
-print(result)
+def regex_examples():
+
+    # the r is for a raw string
+    # the . matches any characters and
+    print(search_pattern(r"l.l", "lol"))
+
+    # use [] to specify a range
+    print(search_pattern(r"[0-9]", "abcd2"))
+
+    # [range of lower letter and upper letter]
+    print(search_pattern(r"[Jj]ava", "java"))
+    print(search_pattern(r"[pP]ython", "Python"))
+
+    # ignore upper lower cases
+    print(search_pattern(r".eans", "Jeans", re.IGNORECASE))
+
+    # (the | is or) to match the first a or b
+    print(search_pattern(r"cat|dog", "there is a cat"))
+
+    # (* qualifier) is used for infinite number of characters or 0
+    print(search_pattern(r"py[a-z]*n", "pytttttthhhhhhoooooooon"))
+    print(search_pattern(r"numbers:[0-9]..*", "numbers:01234567.."))
+
+    # (+ qualifier) for 1 or more characters
+    print(search_pattern(r"a+h!", "aaaaaaah!"))
+    print(search_pattern(r"l+o+l+!", "lllooooooolllll!"))
+
+    # (the ^ is used to match on the start of the line!)
+    print(search_pattern(r"^start", "start, end!"))
+
+    # (the $ is used to match on the end of the line!)
+    print(search_pattern(r"end$", "start, end"))
+
+    # (the ? is used to match the character before it 0 or 1 time)
+    print(search_pattern(r"j?ava", "lava"))
+    print(search_pattern(r"j?ava", "java"))
+
+    # the \ is to escape a character
+    print(search_pattern(r"\.com", "google.com"))
+
+    # \w is used to match any alphabet, numeric, and underscores _ characters only
+    print(search_pattern(r"\w*", "abc089__hehe"))
+
+    # \d is used to match any digits characters only
+    print(search_pattern(r"\d*", "123"))
+
+    # \s is used to match white spaces characters only
+    print(search_pattern(r"\shey*", " hey"))
+
+
+def practice():
+    # method to practice
+    pattern = r"^[a-zA-Z\s]*[0-9]*$"
+
+    print(search_pattern(pattern, "this is valid"))
+    print(search_pattern(pattern, "this is valid 34"))
+    print(search_pattern(pattern, "ThisIsValid"))
+    print(search_pattern(pattern, "  ThisIsValid  "))
+
+
+def main():
+
+    print("Practice: ")
+    practice()
+
+    print("Examples: ")
+    regex_examples()
+
+
+main()
