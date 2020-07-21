4d2a853aa3062a77c4f96d09fe984563869e9aed
Dylan Buchi <devybuchi@gmail.com>
2020-07-21 02:34:06 -0300

    add code to get json data from url

--- network/json_app.py
+++ network/json_app.py
@@ -1,11 +1,19 @@
 import json
+import urllib.request
 
-data = """
-{   "id" : "456d",
-    "number" : "1234567",
-    "name" : "Bob"
-}
-"""
+url = "http://py4e-data.dr-chuck.net/comments_697177.json"
+print('Retrieved', url)
 
-info = json.loads(data)
-print(info)
+url_handle = urllib.request.urlopen(url)
+url_data = url_handle.read()
+print('Retrieved', len(url_data))
+
+data = json.loads(url_data)
+
+result = []
+for key in data['comments']:
+    num = int(key['count'])
+    result.append(num)
+
+print('Count:', len(result))
+print('Sum:', sum(result))
