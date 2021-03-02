15cb35fb46a62359e879959f92f0be39af8b985b
Dylan Buchi <devybuchi@gmail.com>
2021-03-02 05:04:04 -0300

    add add_amount function

--- closures/closures.py
+++ closures/closures.py
@@ -9,15 +9,34 @@ def counter():
     return increment_count
 
 
-def main():
+def add_amount(amount):
+    def inner(n):
+        return amount + n
+    return inner
+
+
+def print_counter():
     counter_1 = counter()
     counter_2 = counter()
 
     print(counter_1())
     print(counter_1())
-    
+
     print(counter_2())
     print(counter_2())
+
+
+def print_add_amount():
+    add_5_to = add_amount(10)
+    add_1_to = add_amount(1)
+
+    print(add_5_to(2))
+    print(add_1_to(10))
+
+
+def main():
+    print_counter()
+    print_add_amount()
 
 
 if __name__ == "__main__":
